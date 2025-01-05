e2376e98eaf07b7d8a84245b7b1d670817a1c916
이현종 <jjong___e@Naver.com>
2025-01-05 14:54:37 +0900

    백엔드 초기세팅
    기술스택
    Spring Boot
    Spring Boot Devtools
    SpringWeb
    Spring Data JPA
    Spring Security
    Lombok

---
+++ vinylist/vinylist/src/main/java/com/vinylist/VinylistApplication.java
@@ -0,0 +1,13 @@
+package com.vinylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylistApplication.class, args);
+	}
+
+}
---
+++ vinylist/vinylist/src/test/java/com/vinylist/VinylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
